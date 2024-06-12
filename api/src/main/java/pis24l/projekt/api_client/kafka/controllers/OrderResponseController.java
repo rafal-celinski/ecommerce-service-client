3e18cb17636a612fe09acba015a247f849b43e50
Mateusz Lukasiewicz <mateuszlu2106@gmail.com>
2024-06-12 04:44:55 +0200

    Implemented changing status in client database when product bought

--- api/src/main/java/pis24l/projekt/api_client/kafka/controllers/OrderResponseController.java
+++ api/src/main/java/pis24l/projekt/api_client/kafka/controllers/OrderResponseController.java
@@ -1,14 +1,25 @@
 package pis24l.projekt.api_client.kafka.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import pis24l.projekt.api_client.kafka.model.OrderResponse;
+import pis24l.projekt.api_client.services.ProductUpdateService;
 
 @Service
 public class OrderResponseController {
+    private final ProductUpdateService productUpdateService;
 
+    @Autowired
+    public OrderResponseController(ProductUpdateService productUpdateService) {
+        this.productUpdateService = productUpdateService;
+    }
     @KafkaListener(topics = "order_responses", groupId = "group_id")
     public void listen(OrderResponse orderResponse) {
-        //
+        if (orderResponse.getStatus().equals("SUCCES")) {
+            productUpdateService.updateProductStatus(orderResponse.getProductId(), "Bought");
+        } else {
+            productUpdateService.updateProductStatus(orderResponse.getProductId(), "Not Available");
+        }
     }
 }
