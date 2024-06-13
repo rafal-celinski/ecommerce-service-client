28bb3dc7d89c4d428293edd635e6996c8de0de5e
Przemysław Walecki <p.walecki12@gmail.com>
2024-06-13 02:03:34 +0200

    PIS-24 Cart Search Implemented

--- api/src/main/java/pis24l/projekt/api_client/services/ProductSearchMongoService.java
+++ api/src/main/java/pis24l/projekt/api_client/services/ProductSearchMongoService.java
@@ -1,10 +1,17 @@
 package pis24l.projekt.api_client.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 import pis24l.projekt.api_client.models.Product;
 import pis24l.projekt.api_client.repositories.mongo.ProductRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -14,19 +21,47 @@ import java.util.stream.StreamSupport;
 public class ProductSearchMongoService {
 
     private final ProductRepository productRepository;
+    private final MongoTemplate mongoTemplate;
 
     @Autowired
-    public ProductSearchMongoService(ProductRepository productRepository) {
+    public ProductSearchMongoService(ProductRepository productRepository, MongoTemplate mongoTemplate) {
         this.productRepository = productRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
-    public List<Product> listAllProducts() {
-        Iterable<Product> productsIterable = productRepository.findAll();
-        return StreamSupport.stream(productsIterable.spliterator(), false)
-                .collect(Collectors.toList());
+
+    public Page<Product> searchProducts(String search, String category, String subcategory, String location, String status, Pageable pageable) {
+        Query query = new Query();
+        List<Criteria> criteriaList = new ArrayList<>();
+
+        if (search != null && !search.isEmpty()) {
+            criteriaList.add(Criteria.where("title").regex(search, "i"));
+        }
+
+        if (location != null && !location.isEmpty()) {
+            criteriaList.add(Criteria.where("location").regex(location, "i"));
+        }
+
+        if (category != null && !category.isEmpty()) {
+            criteriaList.add(Criteria.where("category").is(category));
+        }
+        if (subcategory != null && !subcategory.isEmpty()) {
+            criteriaList.add(Criteria.where("subcategory").is(subcategory));
+        }
+        if (status != null && !status.isEmpty()) {
+            criteriaList.add(Criteria.where("status").is(status));
+        }
+
+        if (!criteriaList.isEmpty()) {
+            query.addCriteria(new Criteria().andOperator(criteriaList.toArray(new Criteria[0])));
+        }
+
+        query.with(pageable);
+
+        List<Product> products = mongoTemplate.find(query, Product.class);
+        long count = mongoTemplate.count(query, Product.class);
+
+        return new PageImpl<>(products, pageable, count);
     }
 
-    public Optional<Product> getProductById(String id) {
-        return productRepository.findById(id);
-    }
 }
